4de0b37744858951eb9df796fe34b5300f01cbc0
uriel zacarias <urielzacarias@gmail.com>
2021-07-08 23:56:58 -0700

    added orders link to navbar

--- client/src/components/Navigation/NavItems/NavItems.js
+++ client/src/components/Navigation/NavItems/NavItems.js
@@ -7,6 +7,7 @@ const navItems = ( props ) => (
         <NavItem link="/home"          exact>Home</NavItem>
         <NavItem link="/shop"           exact>Purchase</NavItem>
         {props.isAuthenticated != null ? <NavItem link="/profile"          >Profile</NavItem> : null}
+        {props.isAuthenticated != null ? <NavItem link="/orders"          >Orders</NavItem> : null}
         {!props.isAuthenticated
             ? <NavItem link="/authentication"   >Cotact &amp; Sign-Up</NavItem>
             : <div className={classes.NavItem}><a  href="/api/logout">Logout</a></div>}
