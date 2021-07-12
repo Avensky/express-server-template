01d109884f62b4e95d23aa2dfb78978690da2d26
root <root@ip-172-26-1-122.us-west-2.compute.internal>
2021-07-12 09:58:35 +0000

    fixed broken link to env variable

--- server/config/prod.js
+++ server/config/prod.js
@@ -16,7 +16,7 @@ module.exports = {
     mongoURI                : process.env.MONGO_URI,
     cookieKey               : process.env.COOKIE_KEY,
 
-    endpointSecret          : process.env.ENDPOINT_SECRET,
+    webhookSecret           : process.env.ENDPOINT_SECRET,
     stripePublishableKey    : process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey         : process.env.STRIPE_SECRET_KEY,
 
